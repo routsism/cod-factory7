720202165ba3375b85bb0783336604d3d47b28dc
Routsis Michail <mike7199@icloud.com>
2025-03-18 19:30:04 +0200

    update

---
+++ src/gr/aueb/cf/ch26/DateTimeServer.java
@@ -0,0 +1,35 @@
+package gr.aueb.cf.ch26;
+
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class DateTimeServer extends Thread {
+
+
+    @Override
+    public void run() {
+        ServerSocket servFd;
+        int serverPort = 13;
+
+        try {
+            servFd = new ServerSocket();
+            servFd.bind(new InetSocketAddress("127.0.0.1", serverPort),100);
+
+            while (true) {
+                Socket connectedFd = servFd.accept();
+                BufferedWriter  bw = new BufferedWriter(new OutputStreamWriter(connectedFd.getOutputStream()));
+                bw.write(new Date().toString());
+                bw.flush();
+                connectedFd.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
